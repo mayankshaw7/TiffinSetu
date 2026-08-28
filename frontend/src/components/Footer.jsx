700d7cc4b302a620b5f2a99e4730aab2680101fd
mayankshaw7 <mayankshaw4001@gmail.com>
2026-08-28 11:15:05 +0530

    Tested CI/CD: frontend change2

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,17 +5,14 @@ const Footer = () => {
     <footer className="bg-gray-800 text-white py-8 text-center mt-auto">
       <div className="max-w-4xl mx-auto px-4">
         <div className="flex flex-wrap justify-center gap-6 mb-4">
-          <span className="text-gray-400">TiffinSetu</span>
+          <span className="text-gray-400">© 2026 TiffinSetu</span>
           <span className="text-gray-600">|</span>
           <span className="text-gray-400">
-            Bridging Taste. Delivering Trust.
+            ALL Right Reserved.
           </span>
         </div>
-        {/* <p className="text-sm text-gray-400">
-          © 2026 TiffinSetu – <span className="font-medium text-gray-200">Mayank Shaw (NIT PATNA) </span>made with ❤️ for tiffin lovers across India.
-        </p> */}
         <p className="text-sm text-gray-400">
-          © 2026 TiffinSetu | Bridging Taste. Delivering Trust. | Designed & developed with ❤️ by{" "}
+          Designed & developed with ❤️ by{" "}
           <a
             href="https://www.linkedin.com/in/mayankshaw20/"
             target="_blank"
